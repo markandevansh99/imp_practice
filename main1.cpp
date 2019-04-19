7d28d56f1c8bf4a07a8331a27fa6d158d1820118
markandevansh99 <500069794@stu.upes.ac.in>
2019-04-19 16:11:06 +0530

    Final by manager 1

---
+++ main1.cpp
@@ -0,0 +1,11 @@
+//Header files and data items
+#include<iostream.h>
+#include<stdlib.h>
+#include<process.h>
+#include<conio.h>
+int push(int[],int&,int);
+int pop(int[],int&);
+void display(int[],int);
+const int size=50;
+
+
