426d68a6b7996f1b9440588dd8dd3b5fd5f2f959
anuragmor <500069361@stu.upes.ac.in>
2019-04-19 16:25:58 +0530

    code part 2

--- main1.cpp
+++ main1.cpp
@@ -61,3 +61,28 @@ cout<<"\n\t________________________________________";
      display(stack,top);
     }
 
+     }
+  if(ch==3)
+  {
+  cout<<"\n The Stack Now is ::\n";
+  display(stack,top);
+  }
+  cout<<"\n\nEnter '1' to perform more operations or '0' to exit ::";
+    cin>>ch;
+    if (ch==0)
+      exit(0);
+ } while (ch==1);
+  getch();
+ }
+ //functions
+ int push(int stack[],int &top,int ele)
+ {
+  if(top==size-1)
+   cout<<"\nOverflow!!";
+   else
+   {
+    top++;
+    stack[top]=ele;
+   }
+ return 0;
+ }
