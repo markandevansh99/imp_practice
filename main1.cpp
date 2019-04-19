5d4260251a8c454aec9048c33d9833a2b8bd85c0
anuragmor <500069361@stu.upes.ac.in>
2019-04-19 16:21:42 +0530

    code part 1 instered

--- main1.cpp
+++ main1.cpp
@@ -1,3 +1,7 @@
+
+
+
+
 //Header files and data items
 #include<iostream.h>
 #include<stdlib.h>
@@ -17,4 +21,43 @@ void main()
 
 
  cout<<"\n\n\t\  MENU FOR OPERATIONS ON A ARRAY STACK ";
+
+cout<<"\n\t________________________________________";
+   cout<<"\n\n1. INSERT ";
+   cout<<"\n2. DELETE ";
+   cout<<"\n3. DISPLAY ";
+   cout<<"\n4. EXIT ";
+ do
+  {
+   cout<<"\n\nEnter your choice :: ";
+   cin>>ch;
+ if(ch==1)
+ {
+  cout<<"\n Enter Item For Insertion::";
+  cin>>item;
+  res=push(stack,top,item);
+  if (res==-1)
+   {
+     cout<<"Overflow!! Stack full\n";
+     exit(0);
+   }
+   cout<<"\n The Stack Now Is:: \n";
+   display(stack,top);
+
+  }
+  if(ch==2)
+  {
+  cout<<"\n Now Deletion of Elements Will Start..\n";
+   res=pop(stack,top);
+   if(res==-1)
+    {
+     cout<<"Underflow Condition!! \n";
+     exit(0);
+    }
+    else
+    {
+     cout<<"\n Element Deleted is::"<<res<<"\n";
+     cout<<"\n The Stack Now is ::\n";
+     display(stack,top);
+    }
 
