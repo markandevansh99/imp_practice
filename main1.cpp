7e1d15a80ca16898b6c0211a01dec777dbede1ad
markandevansh99 <500069794@stu.upes.ac.in>
2019-04-19 16:12:11 +0530

    Final by manager 2

--- main1.cpp
+++ main1.cpp
@@ -8,4 +8,13 @@ int pop(int[],int&);
 void display(int[],int);
 const int size=50;
 
+//modifying and adding main()body
+//Intial main body
+void main()
+{
+ clrscr();
+ int stack[size],item,top=-1,res,ch;
+
+
+ cout<<"\n\n\t\  MENU FOR OPERATIONS ON A ARRAY STACK ";
 
